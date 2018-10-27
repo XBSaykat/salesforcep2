72197addada6483c2729a5ff0b3a50906abcc447
Mehedi Hasan <mehedipy@gmail.com>
2018-10-27 12:22:39 +0600

    MyActivitylist item to activity details

--- app/src/main/java/net/maxproit/salesforce/ActivityDetailsActivity.java
+++ app/src/main/java/net/maxproit/salesforce/ActivityDetailsActivity.java
@@ -1,14 +1,23 @@
 package net.maxproit.salesforce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ActivityDetailsActivity extends AppCompatActivity {
 
-    @Override
+    Intent myActivityItemIntent;
+    int itemPosition;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_details);
+         myActivityItemIntent = getIntent();
+            itemPosition = myActivityItemIntent.getIntExtra(AppConstant.INTENT_KEY, -1);
+
+
+
+
 
 
 
