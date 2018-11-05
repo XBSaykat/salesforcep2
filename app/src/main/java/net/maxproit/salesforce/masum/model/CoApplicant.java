77e73059fbda228015fb31903e9b8a2b56dfae8b
heyrezwan <heyrezwan@gmail.com>
2018-11-05 11:58:44 +0600

    co applicant model

---
+++ app/src/main/java/net/maxproit/salesforce/masum/model/CoApplicant.java
@@ -0,0 +1,13 @@
+package net.maxproit.salesforce.masum.model;
+
+
+public class CoApplicant {
+
+    int id;
+    String name, segment,dateOfBirth, age, districtOfBirth, countryOfBirth, photoIdType, photoIdNo,
+    photoIdIssueDate, eTin, fName, mName, sName, profession, exList, companyName, designation;
+
+
+
+
+}
