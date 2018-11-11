e922a3c09d32bf824d1d22fffa33f3b85b8c8b85
anubd93 <anubd93@gmail.com>
2018-11-12 02:35:01 +0600

    bug fix

--- app/src/main/java/net/maxproit/salesforce/masum/fragment/lead/LeadStageLoanDetailFragment.java
+++ app/src/main/java/net/maxproit/salesforce/masum/fragment/lead/LeadStageLoanDetailFragment.java
@@ -281,6 +281,7 @@ public class LeadStageLoanDetailFragment extends Fragment {
                 listener,
                 year, month, day);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
         dialog.show();
 
     }
