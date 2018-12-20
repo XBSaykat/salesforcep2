9383ced447f1613b5302423fd575c835b56da930
masum <mdmasum.shuvo.cse@gmail.com>
2018-12-20 18:41:41 +0600

    bug fix co applicant,prospect

--- app/src/main/java/net/maxproit/salesforce/masum/utility/ActivityUtils.java
+++ app/src/main/java/net/maxproit/salesforce/masum/utility/ActivityUtils.java
@@ -110,6 +110,7 @@ public class ActivityUtils {
         Intent intent = new Intent(activity, tClass);
         intent.putExtras(bundle);
         activity.startActivity(intent);
+        activity.finish();
     }
 
     public static void invokVisitPlanDetail(Activity activity, Class<?> tClass, net.maxproit.salesforce.masum.model.api.myactivity.Data visitPlan) {
@@ -119,6 +120,7 @@ public class ActivityUtils {
         Intent intent = new Intent(activity, tClass);
         intent.putExtras(bundle);
         activity.startActivity(intent);
+        activity.finish();
     }
 
     public static void toaster(Context context, String text) {
